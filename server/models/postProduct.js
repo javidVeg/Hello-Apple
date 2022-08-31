54654f4e9c9f2a51c5d4cf7865dd0a672804f4fd
Test User <javidx24@icloud.com>
2022-08-31 15:10:53 -0400

    fixing bug

---
+++ server/models/postProduct.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema({
+    product: String,
+    leakedInfo: String,
+    releaseDate: String,
+    name: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likes: { type: [String], default: [] },
+    comments: { type: [String], default: [] },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostProduct = mongoose.model('PostProduct', productSchema);
+
+export default PostProduct;
